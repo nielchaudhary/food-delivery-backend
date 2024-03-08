7ee8d56a401961c814b078ed44eb9737402f4609
Neel Chaudhary <119964539+nielchaudhary@users.noreply.github.com>
2024-03-09 04:03:15 +0530

    added validation

--- src/router/deliveryAgent.router.js
+++ src/router/deliveryAgent.router.js
@@ -3,9 +3,10 @@ const router = require('express').Router();
 // Controller imports
 const createNewAgentController = require('../controller/deliveryAgentcontroller/createNewAgent.controller');
 
-// Middleware imports
-// Validation imports
 
-router.post('/createNewAgent', createNewAgentController);
+// Validation imports
+const newAgentValidate = require('../validation/agentvalidation/createNewAgent.validate')
+
+router.post('/createNewAgent', newAgentValidate, createNewAgentController);
 
 module.exports = router;
