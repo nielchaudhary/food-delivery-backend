4c13a24afc31ab110023e9ebb9c56e461cd0e5a7
Neel Chaudhary <119964539+nielchaudhary@users.noreply.github.com>
2024-03-09 03:41:31 +0530

    added deliveryagent middleware

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ const app = express();
 const dbConnect = require('../src/config/dbConnect')
 const restaurantRoutes = require('./router/restaurantServices.router')
 const userRoutes = require('./router/userServices.router')
+const deliveryAgentRoutes = require('./router/deliveryAgent.router')
 dbConnect();
 app.use(express.json())
 
 app.use('/restaurant', restaurantRoutes)
 app.use('/user',userRoutes )
+app.use('/deliveryAgent' ,deliveryAgentRoutes)
 
 
 app.listen(3000, ()=>{
