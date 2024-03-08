941e7fe75a884950e321accf2e47b0d58e8c20e2
Neel Chaudhary <119964539+nielchaudhary@users.noreply.github.com>
2024-03-08 17:18:07 +0530

    added validation middleware for updateMenu

--- src/router/restaurantServices.router.js
+++ src/router/restaurantServices.router.js
@@ -5,19 +5,24 @@ const router = require('express').Router()
 const addRestaurant = require('../controller/restaurantController/addRestaurant.controller')
 const updateRestaurantStatus = require('../controller/restaurantController/updatestatus.controller')
 
+const updateRestaurantMenu = require('../controller/restaurantController/updateMenu.controller')
+
 
 //middleware imports
 
 
 
 //validation imports
-const addRestaurantValidate = require('../validation/addRestaurant.validate')
-const updateStatusValidate = require('../validation/updatestatus.validate')
+const addRestaurantValidate = require('../validation/restaurantvalidation/addRestaurant.validate')
+const updateStatusValidate = require('../validation/restaurantvalidation/updatestatus.validate')
 
+const updateMenuValidate = require('../validation/restaurantvalidation/updatemenu.validate')
 
 
 router.post('/addRestaurant',addRestaurantValidate, addRestaurant)
 router.put('/updateRestaurantStatus/:restaurantId',updateStatusValidate,updateRestaurantStatus)
+router.put('/updateRestaurantMenu/:restaurantId',updateMenuValidate,updateRestaurantMenu)
+
 
 
 
