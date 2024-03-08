3aab7ff1be7bcbf6bcc5dd57418c0f3ddd49a748
Neel Chaudhary <119964539+nielchaudhary@users.noreply.github.com>
2024-03-09 03:55:12 +0530

    added middleware for order routes

--- src/app.js
+++ src/app.js
@@ -4,12 +4,14 @@ const dbConnect = require('../src/config/dbConnect')
 const restaurantRoutes = require('./router/restaurantServices.router')
 const userRoutes = require('./router/userServices.router')
 const deliveryAgentRoutes = require('./router/deliveryAgent.router')
+const orderRoutes = require('./router/orderdetails.router')
 dbConnect();
 app.use(express.json())
 
 app.use('/restaurant', restaurantRoutes)
 app.use('/user',userRoutes )
 app.use('/deliveryAgent' ,deliveryAgentRoutes)
+app.use('/order',orderRoutes)
 
 
 app.listen(3000, ()=>{
