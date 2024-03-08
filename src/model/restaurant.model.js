c6168892d2f5b24b1aa110115fc0b6670f6b38ec
Neel Chaudhary <119964539+nielchaudhary@users.noreply.github.com>
2024-03-08 17:13:28 +0530

    added changes in model for menu

--- src/model/restaurant.model.js
+++ src/model/restaurant.model.js
@@ -1,42 +1,44 @@
-const mongoose = require('mongoose')
-const {Schema, Types} = mongoose
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-
-const restaurantSchema = new Schema({
-    name : {
-        type : String,
-        required : true,
-
-    },
-    location : {
+const menuItemSchema = new Schema({
+    itemName: {
         type: String,
-        required: true,
-
+        required: true
     },
-    menu: {
-        type: Object,
-        required: true,
-    },
-    contact  : {
-        type: String,
-        required : true,
-
-    },
-
-    rating : {
-        type : String,
-        default : '',
-    },
-    status : {
-        type: String,
-        required : true,
+    price: {
+        type: Number,
+        required: true
     }
-
 });
 
+const restaurantSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    menu: {
+        type: [menuItemSchema],
+        required: true
+    },
+    contact: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: String,
+        default: ''
+    },
+    status: {
+        type: String,
+        required: true
+    }
+});
 
+const RestaurantInfo = mongoose.model('RestaurantInfo', restaurantSchema);
 
-
-const restaurantInfo = mongoose.model('restaurantInfo', restaurantSchema);
-
-module.exports = restaurantInfo;
+module.exports = RestaurantInfo;
