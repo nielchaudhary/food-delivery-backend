27e83bb2a28c4952fc530f2fbcf621f938af4c87
Neel Chaudhary <119964539+nielchaudhary@users.noreply.github.com>
2024-03-09 04:10:37 +0530

    added logic for pushing orderId in restaurant field

--- src/controller/ordercontroller/createorder.controller.js
+++ src/controller/ordercontroller/createorder.controller.js
@@ -1,7 +1,14 @@
 const order = require('../../model/orderdetails.model');
+const restaurant = require('../../model/restaurant.model')
+const {validationResult} = require("express-validator");
 
 const createOrder = async (req, res) => {
     const { restaurantId, items, totalAmount, deliveryAddress, paymentStatus } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
 
     try {
         // Create a new order instance
@@ -14,8 +21,19 @@ const createOrder = async (req, res) => {
         });
 
         const savedOrder = await newOrder.save();
+        const foundRestaurant = await restaurant.findById(restaurantId);
 
-        return res.status(201).json({"New Order Received" : savedOrder});
+        if (!foundRestaurant) {
+            return res.status(404).send("Restaurant not found");
+        }
+
+        // Push the order ID to the pendingOrders array of the restaurant
+        foundRestaurant.pendingOrders.push(savedOrder._id);
+        await foundRestaurant.save();
+
+        return res.status(201).json({ "New Order Received": savedOrder });
+
+
     } catch (error) {
         console.log(error);
         return res.status(500).send('Something went wrong.');
