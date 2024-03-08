1c6bdd03442626c05784cf4cead97110be3375bc
Neel Chaudhary <119964539+nielchaudhary@users.noreply.github.com>
2024-03-08 17:55:20 +0530

    controller for addRating

---
+++ src/controller/userController/addRating.controller.js
@@ -0,0 +1,27 @@
+const restaurant = require('../../model/restaurant.model');
+
+const addRating = async (req, res) => {
+    const restaurantId = req.params.restaurantId;
+    const { rating } = req.body;
+
+    try {
+        const existingRestaurant = await restaurant.findById(restaurantId);
+        if (!existingRestaurant) {
+            return res.status(404).send('Restaurant not found');
+        }
+
+        existingRestaurant.rating.push(rating);
+
+        const sum = existingRestaurant.rating.reduce((acc, curr) => acc + curr, 0);
+        existingRestaurant.averageRating = sum / existingRestaurant.rating.length;
+
+        await existingRestaurant.save();
+
+        return res.status(201).json({ 'Rating added successfully': rating });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send("Something Went Wrong.");
+    }
+};
+
+module.exports = addRating;
