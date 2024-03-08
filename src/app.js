58b4d7dbd0ec247ab3a466f40fa1f1309c0fa6d4
Neel Chaudhary <119964539+nielchaudhary@users.noreply.github.com>
2024-03-08 13:28:04 +0530

    added routes in main file

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express();
 const dbConnect = require('../src/config/dbConnect')
+const restaurantRoutes = require('../src/router/delivery.router')
 dbConnect();
+app.use(express.json())
+
+app.use('/restaurant', restaurantRoutes)
 
 
 app.listen(3000, ()=>{
