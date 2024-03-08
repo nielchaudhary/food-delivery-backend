f87c4254c44c677b68c0eb94c5954f797e56a012
Neel Chaudhary <119964539+nielchaudhary@users.noreply.github.com>
2024-03-08 14:12:17 +0530

    added router for user services

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express();
 const dbConnect = require('../src/config/dbConnect')
-const restaurantRoutes = require('../src/router/delivery.router')
+const restaurantRoutes = require('./router/restaurantServices.router')
+const userRoutes = require('./router/userServices.router')
 dbConnect();
 app.use(express.json())
 
 app.use('/restaurant', restaurantRoutes)
+app.use('/user',userRoutes )
 
 
 app.listen(3000, ()=>{
