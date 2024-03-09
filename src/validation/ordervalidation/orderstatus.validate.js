6da6ef4d73c9fd5061c61d0d02fcd13732e196a0
Neel Chaudhary <119964539+nielchaudhary@users.noreply.github.com>
2024-03-09 14:38:58 +0530

    update: added validation for status update

---
+++ src/validation/ordervalidation/orderstatus.validate.js
@@ -0,0 +1,12 @@
+const { body, param} = require('express-validator');
+
+const acceptOrderValidate = [
+    param('orderId')
+        .notEmpty().withMessage("Order ID is required")
+        .isMongoId().withMessage("Invalid Order ID"),
+    body('status')
+        .notEmpty().withMessage("Status is required")
+        .isIn(['pending', 'accepted', 'rejected', 'completed']).withMessage("Invalid status")
+];
+
+module.exports = acceptOrderValidate;
