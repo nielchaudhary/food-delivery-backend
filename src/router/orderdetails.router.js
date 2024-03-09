469ca69300b605282c68547aeb23c2a836b86e7f
Neel Chaudhary <119964539+nielchaudhary@users.noreply.github.com>
2024-03-09 14:39:46 +0530

    update : added route for updateStatus

--- src/router/orderdetails.router.js
+++ src/router/orderdetails.router.js
@@ -3,11 +3,16 @@ const router = require('express').Router()
 //controller imports
 const createOrderController = require('../controller/ordercontroller/createorder.controller')
 
+const orderDecisionController = require('../controller/ordercontroller/orderStatus.controller')
+
 
 //validation imports
 const createNewOrderValidate = require('../validation/ordervalidation/createNewOrder.validate')
 
+const orderstatusvalidate = require('../validation/ordervalidation/orderstatus.validate')
+
 router.post('/createNewOrder',createNewOrderValidate, createOrderController)
+router.put('/orderDecision/:orderId', orderstatusvalidate, orderDecisionController)
 
 
 
