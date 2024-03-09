46176fcfa9e98f1b386108eddfb78bab673c795e
Neel Chaudhary <119964539+nielchaudhary@users.noreply.github.com>
2024-03-09 19:13:12 +0530

    added validation for orderdeparted.controller

--- src/router/orderdetails.router.js
+++ src/router/orderdetails.router.js
@@ -3,16 +3,20 @@ const router = require('express').Router()
 //controller imports
 const createOrderController = require('../controller/ordercontroller/createorder.controller')
 
-const orderDecisionController = require('../controller/ordercontroller/orderStatus.controller')
+const orderDecisionController = require('../controller/ordercontroller/orderDecision.controller')
+
+const orderdepartedController = require('../controller/ordercontroller/orderdeparted.controller')
 
 
 //validation imports
 const createNewOrderValidate = require('../validation/ordervalidation/createNewOrder.validate')
-
 const orderstatusvalidate = require('../validation/ordervalidation/orderstatus.validate')
+const orderdepartedvalidate = require('../validation/ordervalidation/orderdeparted.validate')
 
 router.post('/createNewOrder',createNewOrderValidate, createOrderController)
 router.put('/orderDecision/:orderId', orderstatusvalidate, orderDecisionController)
+router.post('/orderdeparted/:orderId', orderdepartedvalidate,orderdepartedController)
+
 
 
 
